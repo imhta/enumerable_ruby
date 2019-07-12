921ec33057c161942c20684c44b9ef8414328e8d
imhtop <nponmuthuselvam@gmail.com>
2019-07-12 21:13:11 +0530

    added upto map

---
+++ enumerable.rb
@@ -0,0 +1,95 @@
+module Enumerable
+    def my_each
+        i = 0
+        while i < self.size do
+            yield(self[i])
+            i+=1
+        end
+    end
+
+    def my_each_with_index
+        i = 0
+        while i < self.size do
+            yield(self[i], i)
+            i+=1
+        end
+    end
+
+    def my_select
+        res = []
+        self.my_each do |v|
+          if yield v
+            res << v
+          end
+        end
+        res
+    end
+
+    def my_all?
+        self.my_each {|v| return false unless yield v} 
+        true
+    end
+
+    def my_any?
+        self.my_each {|v| return true if yield v}
+        false
+    end
+
+    def my_none?
+        self.my_each {|v| return false unless yield v}
+        true
+    end
+
+    def my_count
+        res = 0
+        self.my_each {|v|  res+=1 if yield v}
+        res
+    end
+
+    def my_map
+        res = []
+        self.my_each {|v|  res << yield(v) }
+        res
+    end
+
+
+
+end
+
+#input
+arr = [1,2,3,4,5,6]
+
+# #each
+# arr.each {|v| p v}
+# arr.my_each {|v| p v}
+
+# #each_witn_index
+# arr.each_with_index {|v,i| p "#{i} => #{v}"}
+# arr.my_each_with_index {|v,i| p "#{i} => #{v}"}
+
+# #select
+# arr.select {|x| p x}
+# arr.my_select {|x| p x}
+
+# #all
+# p arr.all? {|v| v >= 1} 
+# p arr.my_all? {|v| v >= 1}
+
+# #any?
+# p arr.any? {|v| v >= 2} 
+# p arr.my_any? {|v| v >= 2}
+
+# #my_none?
+# p arr.none? {|v| v >= 2}
+# p arr.my_none? {|v| v >= 2}
+
+# #my_count
+# p arr.count {|v| v >= 2}
+# p arr.my_count {|v| v >= 2}
+
+#my_map
+# new_arr = arr.map {|x| x * 2}
+# my_new_arr = arr.my_map {|x| x * 2}
+# p new_arr, my_new_arr
+
+#my_inject
